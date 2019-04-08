197dc4718e19084450cc988da2ba1637f42ddbea
cammac <camm@rocktechcomputer.com>
2019-04-08 15:39:09 -0700

    initial

---
+++ apple-nav/src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav = props => {
+  return (
+    <div>
+      Nav Item
+    </div>
+  )
+}
+
+export default Nav;
---
+++ apple-nav/src/components/NavHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './Nav';
+
+const NavHeader = props => {
+  return (
+    <div>
+      <h1>Nav Header</h1>
+      <Nav />
+    </div>
+  )
+}
+
+export default NavHeader;
